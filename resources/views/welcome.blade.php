45966227df1777a6502e7aba95114193ad5de064
Chuks <chuksdsilent@gmail.com>
2021-08-14 06:37:30 +0100

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="{{asset("bootstrap/css/bootstrap.css")}}" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="{{asset("style.css")}}" rel="stylesheet" />
 </head>
 
